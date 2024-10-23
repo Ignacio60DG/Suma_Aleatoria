bcf716179a09841770b18b60a76b3428b782503f
Nacho Díez <idiegar2502@g.educaand.es>
2024-10-23 08:48:54 +0200

    Suma :D

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,12 @@ public class Main {
 
         //https://chuidiang.org/index.php?title=Generar_n%C3%BAmeros_aleatorios_en_Java
         //https://codegym.cc/es/groups/posts/es.584.mtodo-java-mathrandom
-        //Math.floor para quitamos los decimales
+        //Math.floor para quitamos los decimales (Aquí no hace falta)
         //Math.random para numero aleatorio entre el 1 y el 0
 
         Scanner sc = new Scanner(System.in);
-        int a = (int)Math.floor(Math.random()*100-1);
-        int b = (int)Math.floor(Math.random()*100-1);
+        int a = (int) (Math.random()*99)+1;
+        int b = (int) (Math.random()*99)+1;
         System.out.println("a es "+a+" b es "+b+", dime la suma de los dos");
         int numero = sc.nextInt();
         int suma = a+b;
